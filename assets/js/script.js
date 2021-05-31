e53fe55177e3390b61eb5c0e693793723d24eb9d
jafet <jafet.manauth@hotmail.com>
2021-05-31 00:36:42 -0500

    made loop function also assign times to the time blocks and color code, but the color code isn't working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,31 @@ renderClock();
 setInterval(renderClock, 1000);
 //loop to create all time blocks
 function timeBlocks(){
-    for (i = 0; i < 9; i++){
+    for (i = 0; i < 24; i++){
         var time = document.createElement("div");
         var txt = document.createElement("textarea");
         var button = document.createElement("button");
+        var hour = moment("09:00:00", "hh:mm:ss a").add( i , 'hours');
+        console.log(hour.format("h a"));
+        console.log(moment().format('h a'));
+        
         $(time).attr("class", "col-12 col-xl-1 hour");
         $(txt).attr("class", "col-12 col-xl-10");
         $(button).attr("class", "col-12 col-xl-1 saveBtn");
         $(".row").append(time);
         $(".row").append(txt);
         $(".row").append(button);
-    }
+        $(time).text(hour.format("h a"));
+
+       if(hour.format("h a") === moment().format("h a")){
+            $(txt).attr('class', 'col-12 col-xl-10 present');
+        } else if (hour.format("h a") < moment().format("h a")){
+            $(txt).attr('class', 'col-12 col-xl-10 past');
+        } else if (hour.format("h a") > moment().format("h a")) {
+            $(txt).attr('class', 'col-12 col-xl-10 future');
+        } 
+    };
 }
+
 
 timeBlocks();

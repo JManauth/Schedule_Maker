858486ef16e547213d53488a6467f1b542dba9c3
jafet <jafet.manauth@hotmail.com>
2021-05-30 23:10:18 -0500

    added date and updating time in header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+//make display day and time uptop
+function renderClock(){
+$("#currentDay").text(moment().format("[Today is: ] dddd, MMMM Do YYYY, h:mm a"));
+};
+renderClock();
+setInterval(renderClock, 1000);
 //loop to create all time blocks
 function timeBlocks(){
-    for (i = 0; i < 8; i++){
+    for (i = 0; i < 9; i++){
         var time = document.createElement("div");
         var txt = document.createElement("textarea");
         var button = document.createElement("button");

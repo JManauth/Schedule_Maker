41f513eec7c1c87b474d98f5265c67d66c015025
jafet <jafet.manauth@hotmail.com>
2021-06-01 09:20:20 -0500

    fixed time frame to 9 - 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,11 @@ setInterval(renderClock, 1000);
 
 //loop to create all time blocks
 function timeBlocks(){
-    for (var i = 0; i < 24; i++){
+    for (var i = 0; i < 9; i++){
         var time = document.createElement("div");
         var txt = document.createElement("textarea");
         var button = document.createElement("button");
-        var hour = moment("00:00", "HH:mm").add( i , 'hours');
+        var hour = moment("09:00", "HH:mm").add( i , 'hours');
         var hourBlock = hour.format('HH')
         var currentHour = moment().format('HH');
         
@@ -42,6 +42,8 @@ function timeBlocks(){
     var box4 = document.getElementById('4');
     var box5 = document.getElementById('5');
     var box6 = document.getElementById('6');
+    var box7 = document.getElementById('7');
+    var box8 = document.getElementById('8');
 
     $('button').click(function(){
             var allBlocks = {
@@ -52,6 +54,8 @@ function timeBlocks(){
                 block4: box4.value.trim(),
                 block5: box5.value.trim(),
                 block6: box6.value.trim(),
+                block7: box7.value.trim(),
+                block8: box8.value.trim(),
             };
     
             localStorage.setItem("allBlocks", JSON.stringify(allBlocks));
@@ -69,7 +73,9 @@ function timeBlocks(){
         document.getElementById("3").innerHTML = lastText.block3;
         document.getElementById("4").innerHTML = lastText.block4;
         document.getElementById("5").innerHTML = lastText.block5;
-        document.getElementById("6").innerHTML = lastText.block6;  
+        document.getElementById("6").innerHTML = lastText.block6;
+        document.getElementById("7").innerHTML = lastText.block7;
+        document.getElementById("8").innerHTML = lastText.block8;  
         } else {
             return;
         }
